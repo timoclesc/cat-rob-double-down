37921d4876015d2efaefc2c14fa9f68d4adff2d1
Tim Copland <tim.copland@august.com.au>
2025-12-09 23:18:14 +1100

    Update footer

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,7 @@ export default async function Page() {
       </main>
       <footer className="bg-stone-800 text-white py-8">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-          <p>&copy; 2025 Against the Odds Challenge. Run for a reason.</p>
+          <p>&copy; 2025 Cat & Rob's Ultra Challenge. Run for a reason.</p>
         </div>
       </footer>
     </div>
